9dc89517d976c6e58ff9ec5e117e2d7fcb0b8388
Angel Zhang <angelzhang159@gmail.com>
2025-03-12 01:29:51 +0100

    Add main components (main, login, register, app)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {LoginComponent} from './components/login/login.component';
+import {MainAppComponent} from './components/main-app/main-app.component';
+import {RegisterComponent} from './components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'app',
+    component: MainAppComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
