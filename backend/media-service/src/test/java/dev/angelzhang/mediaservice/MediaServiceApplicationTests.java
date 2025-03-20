e2e5aafd2568c2c356b0ab204543a4b93f67d5fe
Angel Zhang <angelzhang159@gmail.com>
2025-03-20 22:22:22 +0100

    Add media search service

---
+++ backend/media-service/src/test/java/dev/angelzhang/mediaservice/MediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.angelzhang.mediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
