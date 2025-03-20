96c7c0985c87650ddb2669562d971f468aca63b0
Angel Zhang <angelzhang159@gmail.com>
2025-03-20 22:24:03 +0100

    Config security media service

---
+++ backend/media-service/src/main/java/dev/angelzhang/mediaservice/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package dev.angelzhang.mediaservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http
+                .cors(cors ->
+                        cors.configurationSource(apiConfigurationSource()
+                ))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/media/health", "/media/search/*")
+                        .permitAll());
+        return http.build();
+    }
+
+
+    //Move CORS config to Gateway
+    UrlBasedCorsConfigurationSource apiConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Frontend URL
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed methods
+        configuration.setAllowedHeaders(List.of("*")); // Allow all headers, adjust as needed
+        configuration.setAllowCredentials(true); // Allow credentials (cookies, JWTs)
+
+        // Enable logging to help debug
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Apply CORS to all endpoints
+        return source;
+    }
+}
