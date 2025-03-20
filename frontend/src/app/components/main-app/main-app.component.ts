6ba6f10142481b5fe75019f04255e96f0765b41a
Angel Zhang <angelzhang159@gmail.com>
2025-03-20 22:27:15 +0100

    Add media search box and show it

--- frontend/src/app/components/main-app/main-app.component.ts
+++ frontend/src/app/components/main-app/main-app.component.ts
@@ -1,19 +1,48 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
-import {Router} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs';
+import {MediaService} from '../../services/media.service';
+import {SearchMultiResponse} from '../../models/searchMultiResponse';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-main-app',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NgOptimizedImage,
+    RouterLink
+  ],
   templateUrl: './main-app.component.html',
   styleUrl: './main-app.component.css'
 })
-export class MainAppComponent {
+export class MainAppComponent implements OnInit {
+
   authService = inject(AuthService)
+  mediaService = inject(MediaService)
+
   route = inject(Router)
+  searchControl = new FormControl('');
+  public result: SearchMultiResponse = {
+    page: 0,
+    results: [],
+    total_results: 0,
+    total_pages: 0
+  };
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(query => this.mediaService.searchMulti(query == null ? "" : query))
+    ).subscribe(data => this.result = data)
+  }
 
   logOut() {
     this.authService.logOut()
     this.route.navigate(["/"])
   }
 }
+
