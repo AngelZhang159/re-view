f59d69d6baeffadc7c1ec5b0d2655174ca2c841e
Angel Zhang <angelzhang159@gmail.com>
2025-03-26 00:57:19 +0100

    Add review repository

---
+++ backend/review-service/src/main/java/dev/angelzhang/reviewservice/ReviewRepository.java
@@ -0,0 +1,8 @@
+package dev.angelzhang.reviewservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
