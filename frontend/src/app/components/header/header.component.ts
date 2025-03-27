d1cb84eda6baf892b8df2404a5881be5f420d3c3
Angel Zhang <angelzhang159@gmail.com>
2025-03-27 19:34:04 +0100

    Add header component

---
+++ frontend/src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {enviroment} from '../enviroments/enviroment';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  domain: string = enviroment.domain;
+  domainName: string = enviroment.domainName;
+}
