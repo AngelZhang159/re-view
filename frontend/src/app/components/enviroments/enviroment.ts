ca3a1b0da42d7bf1875130eed62350554533c122
Angel Zhang <angelzhang159@gmail.com>
2025-03-27 19:32:42 +0100

    Add domain to the enviroments

--- frontend/src/app/components/enviroments/enviroment.ts
+++ frontend/src/app/components/enviroments/enviroment.ts
@@ -1,5 +1,7 @@
 export const enviroment = {
   production: false,
   apiUrl: 'http://localhost:8000',
-  apiMediaUrl: 'http://localhost:8001'
+  apiMediaUrl: 'http://localhost:8001',
+  domainName: 're-view.dev',
+  domain: 'https://re-view.dev',
 }
