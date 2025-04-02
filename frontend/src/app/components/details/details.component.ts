e53a4d99a4bd66046dc4f597cc922ce4ccecd56e
Angel Zhang <angelzhang159@gmail.com>
2025-04-02 13:25:27 +0200

    Add details origin and prod countries chip

--- frontend/src/app/components/details/details.component.ts
+++ frontend/src/app/components/details/details.component.ts
@@ -1,13 +1,16 @@
 import {Component, Inject} from '@angular/core';
-import {DetailsResponse} from '../../models/details-response';
+import {CountryCode, DetailsResponse, ProductionCountry} from '../../models/details-response';
 import {MAT_BOTTOM_SHEET_DATA} from '@angular/material/bottom-sheet';
 import {NgOptimizedImage, NgStyle} from '@angular/common';
+import {MatChip, MatChipSet} from '@angular/material/chips';
 
 @Component({
   selector: 'app-details',
   imports: [
     NgOptimizedImage,
-    NgStyle
+    NgStyle,
+    MatChipSet,
+    MatChip
   ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
@@ -22,4 +25,20 @@ export class DetailsComponent {
   details: DetailsResponse;
 
   protected readonly JSON = JSON;
+  protected readonly String = String;
+
+  getCountryFlag(country: string): string {
+    if (country.length !== 2) return '';
+    const offset = 0x1F1E6;
+    const firstChar = country.charCodeAt(0) - 65 + offset;
+    const secondChar = country.charCodeAt(1) - 65 + offset;
+    return String.fromCodePoint(firstChar, secondChar);
+  }
+
+  areCountriesEqual(origin_country: CountryCode[], production_countries: ProductionCountry[]) {
+    return origin_country.length === production_countries.length &&
+      origin_country.every((country, index) => {
+        return country.toString() === production_countries[index].iso_3166_1;
+      });
+  }
 }
