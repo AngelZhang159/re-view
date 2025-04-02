64ce4915c8c2dc091a0ed4f91bfc8b4346713d8a
Angel Zhang <angelzhang159@gmail.com>
2025-04-03 00:19:48 +0200

    Modify review component

--- frontend/src/app/components/review/review.component.ts
+++ frontend/src/app/components/review/review.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Tab, TabList, TabPanel, TabPanels, Tabs} from 'primeng/tabs';
 
 @Component({
   selector: 'app-review',
-  imports: [],
+  imports: [
+    Tabs,
+    TabList,
+    TabPanels,
+    TabPanel,
+    Tab
+  ],
   templateUrl: './review.component.html',
   styleUrl: './review.component.css'
 })
 export class ReviewComponent {
+  tabs = [
+    {
+      title: 'ALL',
+      value: 'all',
+      content: 'All reviews content goes here'
+    },
+    {
+      title: 'MOVIES',
+      value: 'movies',
+      content: 'My reviews content goes here'
+    },
+    {
+      title: 'SERIES',
+      value: 'series',
+      content: 'Friends reviews content goes here'
+    }
+  ];
 
 }
