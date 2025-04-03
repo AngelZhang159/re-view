acc2957650935c35c4a5b0d858434ea20f900b7a
Angel Zhang <angelzhang159@gmail.com>
2025-04-03 21:40:05 +0200

    Modify movie & review entity

--- backend/review-service/src/main/java/dev/angelzhang/reviewservice/entities/MovieReview.java
+++ backend/review-service/src/main/java/dev/angelzhang/reviewservice/entities/MovieReview.java
@@ -1,13 +1,32 @@
 package dev.angelzhang.reviewservice.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import dev.angelzhang.reviewservice.dto.ReviewRequest;
+import dev.angelzhang.reviewservice.enums.Type;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
+import java.time.Instant;
+
+@EqualsAndHashCode(callSuper = true)
 @Entity
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MovieReview extends Review{
 
-    @Id
     private Long movieId;
-    private String movieTitle;
 
+    public static MovieReview fromRequest(Long userId, ReviewRequest reviewRequest) {
+        return MovieReview.builder()
+                .userId(userId)
+                .type(Type.fromString(reviewRequest.type()))
+                .review(reviewRequest.review())
+                .rating(reviewRequest.rating())
+                .movieId(reviewRequest.mediaId())
+                .createdAt(Instant.now())
+                .updatedAt(Instant.now())
+                .build();
+    }
 }
--- backend/review-service/src/main/java/dev/angelzhang/reviewservice/entities/Review.java
+++ backend/review-service/src/main/java/dev/angelzhang/reviewservice/entities/Review.java
@@ -2,20 +2,31 @@ package dev.angelzhang.reviewservice.entities;
 
 import dev.angelzhang.reviewservice.enums.Type;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.time.Instant;
 
 @Entity
 @Data
 @Inheritance(strategy = InheritanceType.JOINED)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Review {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Long userId;
+    @Enumerated(EnumType.STRING)
     private Type type;
+    @Column(nullable = false)
+    private Integer rating;
+    @Column(length = 2048)
+    private String review;
     private Instant createdAt;
     private Instant updatedAt;
 
