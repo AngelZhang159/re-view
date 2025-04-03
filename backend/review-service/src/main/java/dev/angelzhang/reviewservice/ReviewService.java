71417704301f9411dd9b3eb54bc5850f3d2bd21c
Angel Zhang <angelzhang159@gmail.com>
2025-04-03 21:41:03 +0200

    Add review crud service

---
+++ backend/review-service/src/main/java/dev/angelzhang/reviewservice/ReviewService.java
@@ -0,0 +1,101 @@
+package dev.angelzhang.reviewservice;
+
+import dev.angelzhang.reviewservice.dto.ReviewRequest;
+import dev.angelzhang.reviewservice.dto.ReviewResponse;
+import dev.angelzhang.reviewservice.entities.MovieReview;
+import dev.angelzhang.reviewservice.entities.Review;
+import dev.angelzhang.reviewservice.entities.TVReview;
+import dev.angelzhang.reviewservice.enums.Type;
+import dev.angelzhang.reviewservice.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final JwtUtil jwtUtil;
+
+    public ResponseEntity<ReviewResponse> createReview(String token, ReviewRequest reviewRequest) {
+
+        Long userId = jwtUtil.extractUserId(token.substring(7));
+
+        //TODO: Validate mediaId maybe
+        Review review = new Review();
+        if (reviewRequest.type().equals(Type.TV.getLabel())) {
+            review = TVReview.fromRequest(userId, reviewRequest);
+        } else if (reviewRequest.type().equals(Type.MOVIE.getLabel())) {
+            review = MovieReview.fromRequest(userId, reviewRequest);
+        }
+
+        Review savedReview = reviewRepository.save(review);
+
+        ReviewResponse reviewResponse = ReviewResponse.toResponse(savedReview);
+        return ResponseEntity.ok(reviewResponse);
+    }
+
+    public ResponseEntity<ReviewResponse> getReview(String token, Long reviewId) {
+        Review review = findValidateReview(token, reviewId);
+        if (review == null) return ResponseEntity.notFound().build();
+
+        ReviewResponse reviewResponse = ReviewResponse.toResponse(review);
+        return ResponseEntity.ok(reviewResponse);
+    }
+
+    public ResponseEntity<ReviewResponse> deleteReview(String token, Long reviewId) {
+
+        Review review = findValidateReview(token, reviewId);
+
+        if (review == null) return ResponseEntity.notFound().build();
+        ReviewResponse reviewResponse = ReviewResponse.toResponse(review);
+        reviewRepository.deleteById(reviewId);
+
+        return ResponseEntity.ok(reviewResponse);
+    }
+
+    private Review findValidateReview(String token, Long reviewId) {
+        Optional<Review> reviewOptional = reviewRepository.findById(reviewId);
+        if (reviewOptional.isEmpty()) return null;
+
+        Review review = reviewOptional.get();
+        if (!review.getUserId().equals(jwtUtil.extractUserId(token.substring(7)))) return null;
+
+        return review;
+
+    }
+
+    public ResponseEntity<ReviewResponse> updateReview(String token, Long reviewId, ReviewRequest reviewRequest) {
+        Review review = findValidateReview(token, reviewId);
+
+        if (review == null) return ResponseEntity.notFound().build();
+
+        if (reviewRequest.type() != null) review.setType(Type.fromString(reviewRequest.type()));
+        if (reviewRequest.review() != null) {
+            if (reviewRequest.review().isBlank()) {
+                review.setReview(null);
+            } else {
+                review.setReview(reviewRequest.review());
+            }
+        }
+        if (reviewRequest.rating() != null) review.setRating(reviewRequest.rating());
+        if (reviewRequest.seasonsWatched() != null && review instanceof TVReview) {
+            ((TVReview) review).setSeasonsWatched(reviewRequest.seasonsWatched());
+        }
+        if (reviewRequest.mediaId() != null) {
+            if (review instanceof MovieReview) {
+                ((MovieReview) review).setMovieId(reviewRequest.mediaId());
+            } else if (review instanceof TVReview) {
+                ((TVReview) review).setTvId(reviewRequest.mediaId());
+            }
+        }
+        review.setUpdatedAt(Instant.now());
+        reviewRepository.save(review);
+        ReviewResponse reviewResponse = ReviewResponse.toResponse(review);
+        return ResponseEntity.ok(reviewResponse);
+    }
+}
