7407cce5781591003924f4f97c7fcfc9fa2dfa23
Angel Zhang <angelzhang159@gmail.com>
2025-04-03 21:40:44 +0200

    Add review request DTO

---
+++ backend/review-service/src/main/java/dev/angelzhang/reviewservice/dto/ReviewRequest.java
@@ -0,0 +1,20 @@
+package dev.angelzhang.reviewservice.dto;
+
+
+import dev.angelzhang.reviewservice.enums.Type;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+public record ReviewRequest(
+        @NotNull(message = "Type cannot be null (movie or tv)")
+        String type,
+        @NotNull(message = "Media ID cannot be null")
+        Long mediaId,
+        @Length(max = 2048, message = "Review cannot exceed 2048 characters")
+        String review,
+        @NotNull(message = "Rating cannot be null")
+        Integer rating,
+        Integer seasonsWatched
+) {
+}
