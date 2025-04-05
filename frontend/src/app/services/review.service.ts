80acb7046749668a4a7a4156deef881b974503d6
Angel Zhang <angelzhang159@gmail.com>
2025-04-05 17:18:12 +0200

    Add review service

--- frontend/src/app/services/review.service.ts
+++ frontend/src/app/services/review.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {ReviewApiService} from './review-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewService {
 
-  constructor() { }
+  reviewApiService = inject(ReviewApiService);
+
+  createReview(type: string, mediaId: number, review: string, rating: number, seasonsWatched?: number) {
+    return this.reviewApiService.createReview({type: type, mediaId: mediaId, review: review, rating: rating, seasonsWatched: seasonsWatched });
+  }
+
+  deleteReview(reviewId: number) {
+    return this.reviewApiService.deleteReview(reviewId);
+  }
+
+  getReview(reviewId: number) {
+    return this.reviewApiService.getReview(reviewId);
+  }
+
+  updateReview(reviewId: number, type: string, mediaId: number, review: string, rating: number, seasonsWatched?: number) {
+    return this.reviewApiService.updateReview(reviewId, {type: type, mediaId: mediaId, review: review, rating: rating, seasonsWatched: seasonsWatched });
+  }
+
+  getReviews() {
+    return this.reviewApiService.getReviews();
+  }
+
+  getReviewsByUser(userId: number) {
+    return this.reviewApiService.getReviewsByUser(userId);
+  }
 }
