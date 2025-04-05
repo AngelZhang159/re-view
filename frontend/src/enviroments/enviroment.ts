ae592fa9900cb44c0b89d98551ac1daaa346748a
Angel Zhang <angelzhang159@gmail.com>
2025-04-05 17:18:24 +0200

    Add review service url

--- frontend/src/enviroments/enviroment.ts
+++ frontend/src/enviroments/enviroment.ts
@@ -2,6 +2,8 @@ export const enviroment = {
   production: false,
   apiUrl: 'http://localhost:8000',
   apiMediaUrl: 'http://localhost:8001',
+  apiReviewUrl: 'http://localhost:8002',
   domainName: 're-view.dev',
   domain: 'http://localhost:4200',
+
 }
