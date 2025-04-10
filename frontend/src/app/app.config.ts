d0a7ea5099686b5d2016e283a3163d9eac20f88f
Angel Zhang <angelzhang159@gmail.com>
2025-04-10 18:52:01 +0200

    fix: add camel case interceptor to providers

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,13 +7,14 @@ import {requestInterceptor} from './interceptors/request.interceptor';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {providePrimeNG} from 'primeng/config';
 import Material from '@primeng/themes/material';
+import {toCamelCaseInterceptor} from './interceptors/to-camel-case.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(
       withFetch(),
       withInterceptors(
-        [requestInterceptor]
+        [requestInterceptor, toCamelCaseInterceptor]
       )),
     provideZoneChangeDetection(
       {eventCoalescing: true}
