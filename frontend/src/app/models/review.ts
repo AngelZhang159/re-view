e6adb6149dbbc11c6c38ad2e6fb0ce2c80f2aa4f
Angel Zhang <angelzhang159@gmail.com>
2025-04-11 23:58:15 +0200

    feat: add missing types in the review request

--- frontend/src/app/models/review.ts
+++ frontend/src/app/models/review.ts
@@ -5,3 +5,47 @@ export type ReviewRequest = {
   rating: number,
   seasonsWatched?: number,
 }
+
+export type ReviewResponsePage = {
+  content: ReviewResponse[],
+  pageable: Pageable,
+  last: boolean,
+  totalPages: number,
+  totalElements: number,
+  size: number,
+  number: number,
+  sort: Sort,
+  numberOfElements: number,
+  first: boolean,
+  empty: boolean,
+}
+
+export type ReviewResponse = {
+  id: number,
+  userId: string,
+  mediaType: string,
+  mediaId: number,
+  mediaTitle: string,
+  mediaPosterPath: string,
+  review: string,
+  rating: number,
+  seasons: number,
+  seasonsWatched: number,
+  createdAt: string,
+  updatedAt: string,
+}
+
+export type Pageable = {
+  pageNumber: number,
+  pageSize: number,
+  sort: Sort,
+  offset: number,
+  paged: boolean,
+  unpaged: boolean,
+}
+
+export type Sort = {
+  sorted: boolean,
+  unsorted: boolean,
+  empty: boolean,
+}
