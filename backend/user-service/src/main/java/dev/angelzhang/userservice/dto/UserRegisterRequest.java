1db33a7081d3206db18df621f5c83cca09ee086c
Angel Zhang <angelzhang159@gmail.com>
2025-04-14 02:09:57 +0200

    feat: add profilePictureUrl field to UserRegisterRequest

--- backend/user-service/src/main/java/dev/angelzhang/userservice/dto/UserRegisterRequest.java
+++ backend/user-service/src/main/java/dev/angelzhang/userservice/dto/UserRegisterRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotEmpty;
 public record UserRegisterRequest(
     @NotEmpty(message = "Username can not be empty") String username,
     @NotEmpty(message = "Password can not be empty") String password,
-    @NotEmpty(message = "Email can not be empty") String email
+    @NotEmpty(message = "Email can not be empty") String email,
+    String profilePictureUrl
 ) {
 }
