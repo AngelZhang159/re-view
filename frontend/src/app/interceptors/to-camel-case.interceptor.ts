4d6c3c673220bc60d9bdc32dd244764f55160e7c
Angel Zhang <angelzhang159@gmail.com>
2025-04-15 19:50:22 +0200

    feat: add interceptor to transform request body keys to snake_case

--- frontend/src/app/interceptors/to-camel-case.interceptor.ts
+++ frontend/src/app/interceptors/to-camel-case.interceptor.ts
@@ -2,6 +2,11 @@ import {HttpInterceptorFn, HttpResponse} from '@angular/common/http';
 import {map} from 'rxjs';
 
 export const toCamelCaseInterceptor: HttpInterceptorFn = (req, next) => {
+  if (req.body) {
+    const transformedBody = transformKeysToSnakeCase(req.body);
+    req = req.clone({ body: transformedBody });
+  }
+
   return next(req).pipe(
     map(response => {
       if (response instanceof HttpResponse && response.body) {
@@ -11,6 +16,23 @@ export const toCamelCaseInterceptor: HttpInterceptorFn = (req, next) => {
     })
   );
 };
+
+function camelToSnake(s: string): string {
+  return s.replace(/([A-Z])/g, (g) => `_${g[0].toLowerCase()}`);
+}
+
+function transformKeysToSnakeCase(obj: any): any {
+  if (Array.isArray(obj)) {
+    return obj.map(item => transformKeysToSnakeCase(item));
+  } else if (obj !== null && obj && typeof obj === 'object') {
+    return Object.keys(obj).reduce((acc, key) => {
+      const snakeKey = camelToSnake(key);
+      acc[snakeKey] = transformKeysToSnakeCase(obj[key]);
+      return acc;
+    }, {} as any);
+  }
+  return obj;
+}
 
 function snakeToCamel(s: string): string {
   return s.replace(/_([a-z])/g, (g) => g[1].toUpperCase());
