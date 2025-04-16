94d8b428516f83d196468166955ece3e57c81c1d
Angel Zhang <angelzhang159@gmail.com>
2025-04-16 02:01:34 +0200

    refactor: remove unused Material Card imports in review component

--- frontend/src/app/components/review/review.component.ts
+++ frontend/src/app/components/review/review.component.ts
@@ -1,17 +1,12 @@
 import {Component, inject} from '@angular/core';
-import {Tab, TabList, TabPanel, TabPanels, Tabs} from 'primeng/tabs';
 import {ReviewService} from '../../services/review.service';
 import {ReviewResponsePage} from '../../models/review';
-import {MatCard, MatCardContent, MatCardTitle} from '@angular/material/card';
 import {NgOptimizedImage} from '@angular/common';
 import {MenuService} from '../../services/menu.service';
 
 @Component({
   selector: 'app-review',
   imports: [
-    MatCard,
-    MatCardTitle,
-    MatCardContent,
     NgOptimizedImage
   ],
   templateUrl: './review.component.html',
