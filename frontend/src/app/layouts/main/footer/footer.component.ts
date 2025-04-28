e76d7c78bd034dc3ab1f16066750b806ce16b194
Angel Zhang <angelzhang159@gmail.com>
2025-04-28 02:28:55 +0200

    feat: implement footer component with copyright and link

--- frontend/src/app/layouts/main/footer/footer.component.ts
+++ frontend/src/app/layouts/main/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {environment} from '../../../../environments/environment';
+import {MatDivider} from '@angular/material/divider';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    MatDivider
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  date = new Date()
+  protected readonly environment = environment;
 }
