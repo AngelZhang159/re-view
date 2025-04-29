3bf79a2b76bbe8f173f9567ef207f34350c2a8e2
Angel Zhang <angelzhang159@gmail.com>
2025-04-29 20:49:02 +0200

    refactor: remove unused NgOptimizedImage import from review component

--- frontend/src/app/layouts/routes/review/review.component.ts
+++ frontend/src/app/layouts/routes/review/review.component.ts
@@ -1,7 +1,6 @@
 import {Component, inject} from '@angular/core';
 import {ReviewService} from '../../../core/services/review.service';
 import {ReviewResponse, ReviewResponsePage} from '../../../core/models/review';
-import {NgOptimizedImage} from '@angular/common';
 import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
 import {MatIcon} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
@@ -10,7 +9,6 @@ import {Tooltip} from 'primeng/tooltip';
 @Component({
   selector: 'app-review',
   imports: [
-    NgOptimizedImage,
     MatSidenav,
     MatSidenavContainer,
     MatSidenavContent,
