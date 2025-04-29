6aa2e293cc4f5564d7e3bc006b6f11c959f66dd6
Angel Zhang <angelzhang159@gmail.com>
2025-04-29 20:48:55 +0200

    fix: update API URLs in production environment configuration

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUserUrl: 'https://api.re-view.angelzhang.dev',
-  apiMediaUrl: 'https://api.re-view.angelzhang.dev',
-  apiReviewUrl: 'https://api.re-view.angelzhang.dev',
+  apiUserUrl: 'https://api-re-view.angelzhang.dev',
+  apiMediaUrl: 'https://api-re-view.angelzhang.dev',
+  apiReviewUrl: 'https://api-re-view.angelzhang.dev',
   domainName: 're-view.dev',
   domain: 'https://re-view.angelzhang.dev',
 }
