9b355661332f20ac11659b89a5b5d94cea85a244
Angel Zhang <angelzhang159@gmail.com>
2025-05-04 20:55:54 +0200

    feat: enhance review creation and updating logic with conditional handling

--- frontend/src/app/layouts/components/create-review/create-review.component.ts
+++ frontend/src/app/layouts/components/create-review/create-review.component.ts
@@ -9,6 +9,8 @@ import {MatDialogClose} from '@angular/material/dialog';
 import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {ReviewService} from '../../../core/services/review.service';
+import {ReviewResponse} from '../../../core/models/review';
+import {MatBottomSheet} from '@angular/material/bottom-sheet';
 
 @Component({
   selector: 'app-create-review',
@@ -33,40 +35,69 @@ export class CreateReviewComponent {
 
   snackBar = inject(MatSnackBar)
   reviewService = inject(ReviewService)
+  private bottomSheet = inject(MatBottomSheet)
 
-  constructor(@Inject(DIALOG_DATA) public data: { details: DetailsResponse }) {
+  constructor(@Inject(DIALOG_DATA) public data: {
+    details: DetailsResponse,
+    currentReview: ReviewResponse | undefined
+  }) {
     this.details = data.details
-    console.log(JSON.stringify(this.details))
+    this.currentReview = data.currentReview
+    if (this.currentReview) {
+      this.rating = this.currentReview.rating
+      this.reviewText = this.currentReview.review
+      this.updateReview = false
+    } else {
+      this.rating = 10
+      this.reviewText = ''
+      this.updateReview = true
+    }
   }
 
   details: DetailsResponse;
+  currentReview: ReviewResponse | undefined;
 
-  rating = 10;
-  reviewText = '';
+  rating: number;
+  reviewText: string;
+  updateReview: boolean;
 
   closeCreateReviewDialog() {
+    this.bottomSheet.dismiss()
 
-    //TODO show modal on complete or error (snackbar maybe)
-    this.reviewService.createReview(this.details.mediaType, this.details.id, this.reviewText, this.rating, undefined).subscribe({
-      next: (response) => {
-        console.log('Review created successfully:', response);
-        this.snackBar.open('Review created successfully', 'Close', {
-          duration: 2000,
-        });
-      },
-      error: (error) => {
-        console.error('Error creating review:', error);
-        this.snackBar.open('Error creating review: ' + JSON.stringify(error), 'Close', {
-          duration: 2000,
-          politeness: "assertive"
-        });
+    if (this.updateReview) {
+      this.reviewService.createReview(this.details.mediaType, this.details.id, this.reviewText, this.rating, undefined).subscribe({
+        next: (_) => {
+          this.snackBar.open('Review created successfully', 'Close', {
+            duration: 2000,
+          });
+        },
+        error: (error) => {
+          this.snackBar.open('Error creating review: ' + JSON.stringify(error), 'Close', {
+            duration: 2000,
+            politeness: "assertive"
+          });
+        }
+      })
+    } else {
+      if (this.currentReview) {
+        this.reviewService.updateReview(this.currentReview.id, this.currentReview.mediaType, this.currentReview.mediaId, this.reviewText, this.rating).subscribe({
+          next: (_) => {
+            this.snackBar.open('Review updated successfully', 'Close', {
+              duration: 2000,
+            });
+          },
+          error: (error) => {
+            this.snackBar.open('Error updated review: ' + JSON.stringify(error), 'Close', {
+              duration: 2000,
+              politeness: "assertive"
+            });
+          }
+        })
+      } else {
+        this.snackBar.open('Unknown error, should never happen (updating review when there is no previous review)', 'Close', {
+          duration: 2000
+        })
       }
-    })
-  }
-
-  isRated: boolean = this.rating == null;
-
-  checkRated() {
-    this.isRated = this.rating == null;
+    }
   }
 }
