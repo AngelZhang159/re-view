1cb6161c45c3b6c8ecb90b25581b68588a4dfc31
Angel Zhang <angelzhang159@gmail.com>
2025-05-25 11:53:46 +0200

    feat: implement service down route

---
+++ frontend/src/app/layouts/routes/service-down/service-down.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-service-down',
+  imports: [],
+  templateUrl: './service-down.component.html',
+  styleUrl: './service-down.component.css'
+})
+export class ServiceDownComponent {
+
+}
