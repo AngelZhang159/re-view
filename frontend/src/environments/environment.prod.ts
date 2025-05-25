3951d453360a58e53c7c25b500ad20c1fa22f6bc
AngelZhang159 <160955222+AngelZhang159@users.noreply.github.com>
2025-05-25 10:44:22 +0200

    chore: delete frontend/src/environments/environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  apiUserUrl: 'https://api-re-view.angelzhang.dev',
-  apiMediaUrl: 'https://api-re-view.angelzhang.dev',
-  apiReviewUrl: 'https://api-re-view.angelzhang.dev',
-  domainName: 're-view.dev',
-  domain: 'https://re-view.angelzhang.dev',
-}
