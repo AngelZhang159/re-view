6c330e875181cccae03e071835e7b7082d1f794b
Angel Zhang <angelzhang159@gmail.com>
2025-05-25 10:53:36 +0200

    feat: add production environment configuration for API endpoints

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUserUrl: 'https://api-re-view.angelzhang.dev',
+  apiMediaUrl: 'https://api-re-view.angelzhang.dev',
+  apiReviewUrl: 'https://api-re-view.angelzhang.dev',
+  domainName: 're-view.dev',
+  domain: 'https://re-view.angelzhang.dev',
+}
